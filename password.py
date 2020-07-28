dc91104e1a746bc93e93310b95c031cf501db577
jayasuryasurta <53438421+jayasuryasurta@users.noreply.github.com>
2020-07-28 15:00:18 +0530

    Update password.py

--- password.py
+++ password.py
@@ -1,46 +1,26 @@
-pa= str(input('enter a password'))
-sym=str("!@#$%&*()")
-symLen=len(sym)
-strlen=len(pa)
-number="0123456789"
-numLen=len(number)
+sym=str("!@#$%&")
 cck=sck=nck=0
-for a in range(0,strlen):
-    if pa[a].isupper():#capital letter check
-
-        cck=1
-
-
-    for b in range(0,symLen):
-        if sym[b]==pa[a]: #sysbol check
-
-            sck=1
-
-        if number[b] == pa[a]:#number check
-
+while cck<1 | sck<1 | nck<1:
+    pa= str(input('enter a password'))
+    strlen=len(pa)
+    for a in range(0,strlen):
+        if pa[a].isupper():#capital letter check
+            cck=1
+            print('d1')
+        if(pa[a].isdigit()):
             nck=1
+            print('d3')
+        for b in range(0,len(sym)):
+            if sym[b]==pa[a]: #sysbol check
+                sck=1
+                print('d2')
 
 
-
-while strlen>8:
     if cck==1 and sck==1 and nck==1:
         print('password accepted')
         break
     else:
         print('print please enter a capital letter , number,symbol')
-        pa= str(input('enter a password'))
+        continue
 
-        for a in range(0,strlen):
-              if pa[a].isupper():#capital letter check
-
-                 cck=1
-
-
-    for b in range(0,symLen):
-        if sym[b]==pa[a]: #sysbol check
-
-            sck=1
-            if number[b] == pa[a]:#number check
-              nck=1
-              break
 
